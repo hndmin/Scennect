a6ff69e5a5a908c621bc0c10c998c73a3495e546
한동민 <andysjhan2@gmail.com>
2024-06-01 23:09:24 +0900

    wss

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
 class User {
-    constructor(ip, x, y, connectTime) {
-        this.ip = ip;
-        this.x = x;
-        this.y = y;
-        //this.connectTime = connectTime;
+    constructor(id, x, y) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
     }
+  }
+
+// WebSocket URL을 wss로 변경
+const webSocket = new WebSocket("wss://ec2-13-124-8-41.ap-northeast-2.compute.amazonaws.com:9000");
+const userId = Math.random().toString(36).substr(2, 9); // 유저를 구분할 ID
+const usersDiv = document.getElementById("users");
+const userPointers = {};
+
+// 유저 포인터 업데이트
+function updateUserPointer(id, x, y) {
+  if (!userPointers[id]) {
+    const pointer = document.createElement("div");
+    pointer.classList.add("user-pointer");
+    usersDiv.appendChild(pointer);
+    userPointers[id] = pointer;
+  }
+  userPointers[id].style.left = `${x}px`;
+  userPointers[id].style.top = `${y}px`;
 }
 
-const webSocket = new WebSocket("wss://ec2-13-124-8-41.ap-northeast-2.compute.amazonaws.com:9000");
+// 마우스 위치 추적 및 서버로 전송
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+  const user = new User(userId, x, y);
+  webSocket.send(JSON.stringify(user));
+});
 
 // 웹 소켓 연결 이벤트
 webSocket.onopen = function () {
-    alert("웹소켓 서버와 연결에 성공했습니다.");
-
-    // 접속 IP는 서버에서 받아온다고 가정
-    const ip = "0.0.0.0"; // 실제로는 서버로부터 IP를 받아와야 함
-
-    // 현재 시간
-    //const connectTime = new Date().toISOString();
-
-    // 마우스 위치 추적
-    let mouseX = 0;
-    let mouseY = 0;
-    document.addEventListener("mousemove", (event) => {
-      mouseX = event.clientX;
-      mouseY = event.clientY;
-    });
-
-    // User 객체 생성
-    const user = new User(ip, mouseX, mouseY);
-
-    // User 객체를 서버로 전송
-    webSocket.send(JSON.stringify(user));
+  console.log("웹소켓 서버와 연결에 성공했습니다.");
 };
 
 // 웹 소켓 메세지 수신
 webSocket.onmessage = function (event) {
-    alert(event.data);
+  const users = JSON.parse(event.data);
+  users.forEach(user => {
+    updateUserPointer(user.id, user.x, user.y);
+  });
 };
 
 // 웹 소켓 연결 종료
 webSocket.onclose = function () {
-    alert("웹소켓 서버와 연결이 종료되었습니다.");
+  console.log("웹소켓 서버와 연결이 종료되었습니다.");
 };
 
 // 오류 발생
 webSocket.onerror = function (error) {
-    console.log(error);
+  console.log(error);
 };
-
-function sendMessage() {
-    const message = document.getElementById("message").value;
-    webSocket.send(message);
-}
-
-
-
-
